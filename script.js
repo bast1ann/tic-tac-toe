c2e6ee34130c12a2d593bde2f9e090f7d8bbbf9a
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-06-21 00:40:42 -0400

    Modify console version of the game
    I tried to apply module patterns.

--- script.js
+++ script.js
@@ -1,109 +1,123 @@
-const board = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ];
+const board = function() {
+  const board = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ];
 
-function createPlayer(name, symbol, isCurrentPlayer) {
-  return {name, symbol, isCurrentPlayer};
-}
-
-function displayBoard() {
-  for (let i = 0; i < board.length; i++) {
-    console.log(board[i]);
+  function displayBoard() {
+    for (let i = 0; i < board.length; i++) {
+      console.log(board[i]);
+    }
   }
-}
 
-function checkEmptyCell(r, c) {
-  return (board[r][c] === 0 ? true : false);
-}
+  function checkEmptyCell(row, col) {
+    return (board[row][col] === 0 ? true : false);
+  }
 
-function checkFullBoard() {
-  for (let row = 0; row < 3 ; row++) {
-    for (let col = 0; col < 3; col++) {
-      if (board[row][col] === 0) {
-        return false;
+  function checkFullBoard() {
+    for (let row = 0; row < 3 ; row++) {
+      for (let col = 0; col < 3; col++) {
+        if (board[row][col] === 0) {
+          return false;
+        }
       }
     }
-  }
-  return true;
-}
-
-function checkWinner(player) {
-  // check row winner
-  for (let row = 0; row < 3; row++) {
-    let counter = 0;
-    for (let col = 0; col < 3; col++) {
-      if (board[row][col] === player.symbol) {
-        counter+= 1;
-      }
-    }
-    if (counter == 3) {
-      return true;
-    }
-  }
-
-  // check column winner
-  for (let col = 0; col < 3; col++) {
-    let counter = 0;
-    for (let row = 0; row < 3; row++) {
-      if (board[row][col] === player.symbol) {
-        counter+= 1;
-      }
-    }
-    if (counter == 3) {
-      return true;
-    }
-  }
-
-  // check diagonal winner
-  if ( ((player.symbol === board[0][0])
-        && (board[0][0] === board[1][1])
-        && (board[1][1] === board[2][2]))
-      || ((player.symbol === board[2][0])
-            && (board[2][0] === board[1][1])
-            && (board[1][1] === board[0][2])) ) {
     return true;
   }
-}
 
-function playMove(r, c, player) {
-  board[r][c] = player.symbol;
-}
-
-function playRound() {
-  let currentPlayer;
-
-  if (playerOne.isCurrentPlayer === true) {
-    currentPlayer = playerOne;
-  }
-  else {
-    currentPlayer = playerTwo;
+  function writeBoard(row, col, symbol) {
+    board[row][col] = symbol;
   }
 
-  let isEmpty;
-  let moveRow;
-  let moveCol;
-
-  do {
-    moveRow = prompt("Row:");
-    moveCol = prompt("Col:");
-    isEmpty = checkEmptyCell(moveRow, moveCol);
-  } while (isEmpty === false);
-
-  playMove(moveRow, moveCol, currentPlayer)
-  displayBoard();
-
-  playerOne.isCurrentPlayer = !playerOne.isCurrentPlayer;
-  playerTwo.isCurrentPlayer = !playerTwo.isCurrentPlayer;
-
-  if (checkWinner(playerOne)) {
-    console.log("Player one is the winner");
+  function getBoard() {
+    return board;
   }
-  else if (checkWinner(playerTwo)) {
-    console.log("Player two is the winner");
-  }
-  else if (checkFullBoard()) {
-    console.log("It's a tie");
-  }
-}
 
-const playerOne = createPlayer(prompt("Player 1:"), 1, true);
-const playerTwo = createPlayer(prompt("Player 2:"), 2, false);
-displayBoard();
+  function resetBoard() {
+    for (let i = 0; i < 3; i++) {
+      board[i].fill(0);
+    }
+  }
+
+  return {displayBoard, checkEmptyCell, checkFullBoard, getBoard, resetBoard, writeBoard};
+};
+
+const gameController = (function() {
+  // initial state
+  const board = board();
+  const playerOne = createPlayer("Player One", 1);
+  const playerTwo = createPlayer("Player Two", 2);
+  let activePlayer = playerOne;
+
+  // functions
+  function createPlayer(name, symbol) {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    
+    return {getName, getSymbol};
+  }
+
+  function switchPlayer() {
+    activePlayer === playerOne ? activePlayer = playerTwo : activePlayer = playerOne;
+  }
+
+  function checkWinner(symbol) {
+    // check row winner
+    for (let row = 0; row < 3; row++) {
+      let counter = 0;
+      for (let col = 0; col < 3; col++) {
+        if (board[row][col] === symbol) {
+          counter+= 1;
+        }
+      }
+      if (counter == 3) {
+        return true;
+      }
+    }
+    // check column winner
+    for (let col = 0; col < 3; col++) {
+      let counter = 0;
+      for (let row = 0; row < 3; row++) {
+        if (board[row][col] === symbol) {
+          counter+= 1;
+        }
+      }
+      if (counter == 3) {
+        return true;
+      }
+    }
+    // check diagonal winner
+    if ( ((symbol === board[0][0])
+    && (board[0][0] === board[1][1])
+    && (board[1][1] === board[2][2]))
+    || ((symbol === board[2][0])
+    && (board[2][0] === board[1][1])
+    && (board[1][1] === board[0][2])) ) {
+      return true;
+    }
+  }
+
+  function playRound() {
+    // get play
+    const [rowPlay, colPlay] = [prompt("Row:"), prompt("Col:")];
+    // check if the cell is empty for the given play
+    if (board.checkEmptyCell(rowPlay, colPlay)) {
+      board.writeBoard(rowPlay, colPlay, activePlayer.getSymbol());
+      board.displayBoard();
+      // check winner or tie
+      if (checkWinner(board.getBoard(), activePlayer.getSymbol())) {
+        console.log(`The winner is ${activePlayer.getName()}!`);
+        // show Modal Dialog and reset button
+      }
+      else if (board.checkFullBoard()) {
+        console.log("It's a tie!");
+        // show Modal Dialog and reset button
+      }
+      else {
+        switchPlayer();
+      }
+    }
+    else {
+      console.log("Invalid move");
+    }
+  }
+
+  return {playRound};
+})();
