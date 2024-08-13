3c4590dd0524deac46919e7ef2d6f42ec0ec5ddd
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-08-13 16:44:29 -0400

    Add UI function that interacts with gameController
    The game can be played from the UI.

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const gameboard = (function() {
-  const board = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ];
-
-  function displayBoard() {
-    for (let i = 0; i < board.length; i++) {
-      console.log(board[i]);
-    }
-  }
+  const board = [ ["", "", ""], ["", "", ""], ["", "", ""] ];
 
   function checkEmptyCell(row, col) {
-    return (board[row][col] === 0 ? true : false);
+    return (board[row][col] === "" ? true : false);
   }
 
   function checkFullBoard() {
     for (let row = 0; row < 3 ; row++) {
       for (let col = 0; col < 3; col++) {
-        if (board[row][col] === 0) {
+        if (board[row][col] === "") {
           return false;
         }
       }
@@ -32,18 +26,19 @@ const gameboard = (function() {
 
   function resetBoard() {
     for (let i = 0; i < 3; i++) {
-      board[i].fill(0);
+      board[i].fill("");
     }
   }
 
-  return {displayBoard, checkEmptyCell, checkFullBoard, getBoard, resetBoard, writeBoard};
+  return {checkEmptyCell, checkFullBoard, getBoard, resetBoard, writeBoard};
 })();
 
 const gameController = (function() {
   // initial state
-  const playerOne = createPlayer("Computer 1", 1);
-  const playerTwo = createPlayer("Computer 2", 2);
+  const playerOne = createPlayer("Computer 1", "X");
+  const playerTwo = createPlayer("Computer 2", "O");
   let activePlayer = playerOne;
+  let message = "";
 
   // functions
   function createPlayer(name, symbol) {
@@ -54,6 +49,14 @@ const gameController = (function() {
     const getScore = () => score;
     
     return {getName, getSymbol, getScore, addScore};
+  }
+
+  function getActivePlayer() {
+    return activePlayer.getName();
+  }
+
+  function getMessage() {
+    return message;
   }
 
   function switchPlayer() {
@@ -100,27 +103,59 @@ const gameController = (function() {
     // check if the cell is empty for the given play
     if (gameboard.checkEmptyCell(row, col)) {
       gameboard.writeBoard(row, col, activePlayer.getSymbol());
-      gameboard.displayBoard();
       // check winner or tie
       if (checkWinner(gameboard.getBoard(), activePlayer.getSymbol())) {
         activePlayer.addScore();
         gameboard.resetBoard();
-        console.log(`The winner is ${activePlayer.getName()}!`);
-        console.log(`${playerOne.getName()} (${playerOne.getScore()}) - (${playerTwo.getScore()}) ${playerTwo.getName()}`);
-        // show Modal Dialog and reset button
+        message = `${activePlayer.getName()} wins this round!`;
       }
       else if (gameboard.checkFullBoard()) {
-        console.log("It's a tie!");
-        // show Modal Dialog and reset button
+        gameboard.resetBoard();
+        message = `It's a tie!`;
       }
       else {
         switchPlayer();
+        message = `It's ${activePlayer.getName()} turn.`;
       }
     }
     else {
-      console.log("Invalid move");
+      message = `This is an invalid move! Play again ${activePlayer.getName()}.`;
     }
   }
 
-  return {playRound};
+  return {playRound,
+          getActivePlayer,
+          getMessage,
+          playerOne : {getName : playerOne.getName, getScore : playerOne.getScore},
+          playerTwo : {getName : playerTwo.getName, getScore : playerTwo.getScore}};
 })();
+
+const gameUI = (function() {
+  const cell = document.querySelectorAll(".cell");
+  const playerOneName = document.querySelector(".player-one-name");
+  const playerTwoName = document.querySelector(".player-two-name");
+  const playerOneScore = document.querySelector(".score-player-one");
+  const playerTwoScore = document.querySelector(".score-player-two");
+  const message = document.getElementById("message");
+
+  playerOneName.textContent = gameController.playerOne.getName();
+  playerTwoName.textContent = gameController.playerTwo.getName();
+
+  cell.forEach( (el) => el.addEventListener("click", () => {
+    const row = parseInt(el.dataset.row);
+    const col = parseInt(el.dataset.col);
+    gameController.playRound(row, col);
+    updateScreen();
+  }) );
+
+  function updateScreen() {
+    cell.forEach( (el) => {
+      el.textContent = gameboard.getBoard()[el.dataset.row][el.dataset.col];
+    } );
+    playerOneScore.textContent = gameController.playerOne.getScore();
+    playerTwoScore.textContent = gameController.playerTwo.getScore();
+    message.textContent = gameController.getMessage();
+  }
+
+})();
+
